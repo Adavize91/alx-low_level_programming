8c5979553d247aadb769824f22cf92bbffed4040
adavize91 <suleimanabdulganiyuadavize.com>
2022-12-20 11:04:05 -0800

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void print_rev(char *s);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void print_array(int *a, int n);
 
 
 
